8fa7b9349af042e8db6c426f8be9dd56ad35c804
Shashank B C <bcshashank140@gmail.com>
2025-02-19 01:15:23 +0545

    Create demo.js

---
+++ Modules(multiple)/demo.js
@@ -0,0 +1,16 @@
+
+const add= (a, b) => {
+    return a + b;
+};
+
+const product= (a, b) => {
+    return a * b;
+}
+
+const pi= 3.14;
+
+module.exports= {add, product, pi};
+ 
+// alternative:
+// module.exports.add= add;
+// module.exports.product= product;
